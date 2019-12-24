841f08e8dbd5d6ecff3b8d6d958dbb45293d15ee
nsohit <52670389+nsohit@users.noreply.github.com>
2019-12-24 14:22:19 +0700

    Add files via upload

---
+++ baca_alamat_ds.ino
@@ -0,0 +1,24 @@
+#include <OneWire.h>
+ 
+OneWire ds(A0);
+void setup(void) {
+  Serial.begin(9600);
+}
+ 
+void loop(void) {
+  byte i;
+  byte addr[8];
+ 
+  if (!ds.search(addr)) {
+    Serial.println(" No more addresses.");
+    Serial.println();
+    ds.reset_search();
+    delay(250);
+    return;
+  }
+  Serial.print(" ROM =");
+  for (i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+}
